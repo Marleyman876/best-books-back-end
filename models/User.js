944945a6bbdb1ad6f68fbd437c3c5e051443b8ae
Shelby Harner <shelby.harner@gmail.com>
2021-04-28 23:14:03 -0700

    complete lab 12 card 2

--- models/User.js
+++ models/User.js
@@ -1,19 +1,20 @@
 'use strict'
 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const bookSchema = new Schema({
   name: String,
+  author: String,
   description: String,
   status: String
-})
+});
 
 const userSchema = new Schema({
   email: String,
   books:[bookSchema]
-  
 });
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 'use strict'
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config()
 const mongoose = require('mongoose');
+app.use(cors());
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -13,31 +15,31 @@ const booksWeLove = new User({
   email: 'shelby.harner@gmail.com',
   books: [
     {
-      title: 'The subtle art of not giving a F*ck',
+      name: 'The subtle art of not giving a F*ck',
       author: 'Mark Manson',
       description: 'A counterintuitive approach to a good life.'
     },
 
     {
-      title: '48 Laws of Power',
+      name: '48 Laws of Power',
       author: 'Robert Green',
       description: 'A moral, cunning, ruthless and instructive way to achieve power.'
     },
 
     {
-      title: 'Five Love Languages',
+      name: 'Five Love Languages',
       author: 'Gary Chapman',
       description: 'The secret to love that lasts.'
     },
 
     {
-      title: 'Markus Garvy: Life Lessons',
+      name: 'Markus Garvy: Life Lessons',
       author: 'Robert A. Hill',
       description: 'Autobiography about the man and the movement'
     },
 
     {
-      title: 'The Wise Mind of H.I.M Emperor Halie Sellassie I',
+      name: 'The Wise Mind of H.I.M Emperor Halie Sellassie I',
       author: 'Asheda Dwyer',
       description: 'Reflections on how to develop ones self'
     },
@@ -45,16 +47,23 @@ const booksWeLove = new User({
 });
 
 booksWeLove.save();
-console.log(booksWeLove);
+// console.log(booksWeLove);
 
 app.get('/', (request, response) => {
   response.send('These are our cool books!');
 });
 
 app.get('/books', (request, response) => {
-  User.find((databaseResults) => {
+  User.find((error, databaseResults) => {
+    console.log(databaseResults);
+    if (error) return console.error(error)
     response.send(databaseResults);
   })
-})
+});
+
+// app.get('/books', (request, response) => {
+//   let results = User.find({})
+//   response.send(results)
+// })
 
 app.listen(3001, () => console.log('listening on 3001'));
